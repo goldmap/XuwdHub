8b8ec9496ce56466c9fc9c0ca6a287141b8bcc0e
goldmap <xuwd68@qq.com>
2019-07-08 19:05:30 +0800

    tt

--- FileManager/app/src/main/java/com/zyj/filemanager/bean/FileType.java
+++
@@ -1,9 +0,0 @@
-package com.zyj.filemanager.bean;
-
-/**
- * Created by ${zhaoyanjun} on 2017/1/11.
- */
-
-public enum  FileType {
-    directory , txt , zip , video , music , image ,apk , other
-}
