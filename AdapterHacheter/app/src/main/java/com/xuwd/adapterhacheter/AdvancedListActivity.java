04bdea917ba62ce643426f2dcbce3a78c3b62e34
goldmap <xuwd68@qq.com>
2019-07-10 18:16:28 +0800

    click

--- AdapterHacheter/app/src/main/java/com/xuwd/adapterhacheter/AdvancedListActivity.java
+++ AdapterHacheter/app/src/main/java/com/xuwd/adapterhacheter/AdvancedListActivity.java
@@ -2,13 +2,17 @@ package com.xuwd.adapterhacheter;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+//extends AdapterView.OnItemClickListener
 public class AdvancedListActivity extends ListActivity {
     // private List<String> data = new ArrayList<String>();
     @Override
@@ -19,6 +23,16 @@ public class AdvancedListActivity extends ListActivity {
                 new String[]{"title","info","img"},
                 new int[]{R.id.title,R.id.info,R.id.img});
         setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        TextView tv=v.findViewById(R.id.info);
+        String inf=tv.getText().toString();
+        Map m=(Map) l.getAdapter().getItem(position);
+        String title=m.get("title").toString();
+        Toast.makeText(this,title+","+inf,Toast.LENGTH_SHORT).show();
     }
 
     private List<Map<String, Object>> getData() {
