5fe50ea6f4ecd331d9d4d43b9d402fe45eacae1c
goldmap <42493201+goldmap@users.noreply.github.com>
2019-07-11 23:31:22 +0800

    re

---
+++ JRecycleView/app/src/main/java/com/xuwd/jrecycleview/MainActivity.java
@@ -0,0 +1,22 @@
+package com.xuwd.jrecycleview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private String demos[]={"A","B"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.list_simple_central,R.id.simpleList, demos);
+        ListView listView=findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+    }
+}
