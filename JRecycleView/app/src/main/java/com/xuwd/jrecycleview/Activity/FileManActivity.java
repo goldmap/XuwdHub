46106368430238576b53e32346097357c6cd41f9
goldmap <42493201+goldmap@users.noreply.github.com>
2019-07-16 00:05:30 +0800

    j

--- JRecycleView/app/src/main/java/com/xuwd/jrecycleview/Activity/FileManActivity.java
+++ JRecycleView/app/src/main/java/com/xuwd/jrecycleview/Activity/FileManActivity.java
@@ -5,6 +5,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.xuwd.jrecycleview.Adapter.RecycleAdapter;
 import com.xuwd.jrecycleview.R;
@@ -12,7 +17,8 @@ import com.xuwd.jrecycleview.R;
 import java.util.ArrayList;
 
 public class FileManActivity extends AppCompatActivity {
-
+    private ArrayAdapter<String> mListAdapter;
+    private ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,16 +29,36 @@ public class FileManActivity extends AppCompatActivity {
         RecyclerView.LayoutManager horizontalLayoutManager=new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false);
         dirRcyclerView.setLayoutManager(horizontalLayoutManager);
 
-        RecycleAdapter adapter=new RecycleAdapter(R.layout.list_dir_navigator,initData());
-//        RecycleAdapter adapter=new RecycleAdapter(R.layout.list_simple_text,initData());
-        dirRcyclerView.setAdapter(adapter);
+        RecycleAdapter mDirNavigatorAdapter=new RecycleAdapter(R.layout.list_dir_navigator,initData(0));
+        mDirNavigatorAdapter.setOnItemClickListener(new RecycleAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+               reList(position);
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+
+            }
+        });
+//        RecycleAdapter mDirNavigatorAdapter=new RecycleAdapter(R.layout.list_simple_text,initData());
+        dirRcyclerView.setAdapter(mDirNavigatorAdapter);
+
+        mListView =findViewById(R.id.fileListView);
+        //mListAdapter =new ArrayAdapter<String>(this,R.layout.list_pure_text,R.id.listItemText,initData(0));
+        //mListView.setAdapter(mListAdapter);
     }
 
-    public ArrayList<String> initData(){
+    public ArrayList<String> initData(int iStart){
         ArrayList<String> data=new ArrayList<String>();
-        for(int i=0;i<20;i++){
+        for(int i=iStart;i<20;i++){
             data.add("item"+i);
         }
         return data;
+    }
+
+    public void reList(int position){
+        mListAdapter =new ArrayAdapter<String>(this,R.layout.list_pure_text,R.id.listItemText,initData(position));
+        mListView.setAdapter(mListAdapter);
     }
 }
