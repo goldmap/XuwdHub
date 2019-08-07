7a1dbe46ecd554a63f8977a61935153d94875ae9
goldmap <42493201+goldmap@users.noreply.github.com>
2019-08-07 22:56:44 +0800

    location

--- JMap/app/src/main/java/com/xuwd/jmap/JActivity.java
+++ JMap/app/src/main/java/com/xuwd/jmap/JActivity.java
@@ -27,7 +27,6 @@ public class JActivity extends AppCompatActivity {
             Manifest.permission.ACCESS_WIFI_STATE,
             Manifest.permission.CHANGE_WIFI_STATE,
             Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.WRITE_SETTINGS,
     };
     public void initPermission(){
         if(!hasPermissions(VIDEO_PERMISSIONS)){
--- JMap/app/src/main/java/com/xuwd/jmap/MainActivity.java
+++ JMap/app/src/main/java/com/xuwd/jmap/MainActivity.java
@@ -2,7 +2,10 @@ package com.xuwd.jmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.baidu.location.BDLocation;
@@ -18,11 +21,7 @@ import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
 
 public class MainActivity extends JActivity {
-    public LocationClient locationClient;
-    private TextView positionText;
-    private MapView mapView;
-    private BaiduMap baiduMap;
-    private boolean isFirstLocate=true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,53 +29,19 @@ public class MainActivity extends JActivity {
         SDKInitializer.initialize(getApplicationContext());
 
         setContentView(R.layout.activity_main);
-
         initPermission();
-        locationClient=new LocationClient(this);
-        locationClient.registerLocationListener(new BDLocationListener() {
+
+        Button btnMap=findViewById(R.id.btnOpenMap);
+        btnMap.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onReceiveLocation(BDLocation location) {
-                if(location.getLocType()==BDLocation.TypeGpsLocation||location.getLocType()==BDLocation.TypeNetWorkLocation){
-                    navigateTo(location);
-                    }
+            public void onClick(View view) {
+                Intent intent=new Intent(getBaseContext(),MapActivity.class);
+                startActivity(intent);
             }
         });
 
-        mapView=findViewById(R.id.mapView);
-        baiduMap=mapView.getMap();
-        baiduMap.setMyLocationEnabled(true);
-
-        positionText=findViewById(R.id.positionTextView);
-
     }
 
-    private void navigateTo(BDLocation location){
-        if(isFirstLocate){
-            LatLng ll=new LatLng(location.getLatitude(),location.getLongitude());//LatLng类用于存放经纬度
-            // 第一个参数是纬度值，第二个参数是精度值。这里输入的是本地位置。
-            MapStatusUpdate update= MapStatusUpdateFactory.newLatLng(ll);//将LatLng对象传入
-            baiduMap.animateMapStatus(update);
-            update=MapStatusUpdateFactory.zoomTo(16f);//百度地图缩放范围，限定在3-19之间，可以去小数点位值
-            // 值越大，地图显示的信息越精细
-            baiduMap.animateMapStatus(update);
-            isFirstLocate=false;//防止多次调用animateMapStatus()方法，以为将地图移动到我们当前位置只需在程序
-            // 第一次定位的时候调用一次就可以了。
-        }
-        MyLocationData.Builder locationBuilder=new MyLocationData.Builder();
-        locationBuilder.latitude(location.getLatitude());
-        locationBuilder.longitude(location.getLongitude());
-        MyLocationData locationData=locationBuilder.build();
-        baiduMap.setMyLocationData(locationData);//获取我们的当地位置
-    }
-    private void initLocation() {
-        LocationClientOption option=new LocationClientOption();
-        option.setScanSpan(5000);//表示每5秒更新一下当前位置
-        option.setIsNeedAddress(true);
-        option.setLocationMode(LocationClientOption.LocationMode.Device_Sensors);
-        // Hight_Accuracy表示高精确度模式，会在GPS信号正常的情况下优先使用GPS定位，在无法接收GPS信号的时候使用网络定位。
-        // Battery_Saving表示节电模式，只会使用网络进行定位。
-        // Device_Sensors表示传感器模式，只会使用GPS进行定位。
-        locationClient.setLocOption(option);
-    }
+
 
 }
