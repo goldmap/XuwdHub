9dd8c30ce66de04ed89925e135bb35ae10e43c1c
goldmap <42493201+goldmap@users.noreply.github.com>
2019-08-27 00:11:49 +0800

    thread

--- ThreadTest/app/src/main/java/com/xuwd/threadtest/MainActivity.java
+++ ThreadTest/app/src/main/java/com/xuwd/threadtest/MainActivity.java
@@ -16,7 +16,8 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView textView;
-    private Handler mHandler;
+    private MainHandler mainHandler;
+    private JThread jThread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,59 +26,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textView=findViewById(R.id.textView);
         Log.d("AAA", "main: "+Thread.currentThread().getId());
 
-        /*
-        Timer timer=new Timer();
-        TimerTask timerTask=new TimerTask() {
-            @Override
-            public void run() {
-                Log.d("AAA", " timer run:"+Thread.currentThread().getId());
-            }
-        };
-        timer.schedule(timerTask,0,10000);*/
+        Button btnMainToSub=findViewById(R.id.btnMainToSub);
+        btnMainToSub.setOnClickListener(this);
+        Button btnSubToMain=findViewById(R.id.btnSubToMain);
+        btnSubToMain.setOnClickListener(this);
 
-        Button btnThread=findViewById(R.id.btnThread);
-        btnThread.setOnClickListener(this);
+        mainHandler=new MainHandler();
 
-        mHandler=new Handler(){
-            @Override
-            public void handleMessage(@NonNull Message msg) {
-                super.handleMessage(msg);
-                Log.d("AAA", "handleMessage from: "+msg.obj+" ,"+msg.arg1+"thread:"+Thread.currentThread().getId());
-            }
-        };
+        jThread=new JThread("J_Thread");
+        jThread.start();
     }
 
     @Override
     public void onClick(View view) {
         switch(view.getId()){
-            case R.id.btnThread:
-                JThread thread1=new JThread("BBB");
-                JThread thread2=new JThread("CCC");
-                thread1.start();
-                thread2.start();
+            case R.id.btnMainToSub:
+
+                break;
+            case R.id.btnSubToMain:
                 break;
         }
     }
 
     class JThread extends Thread{
         String name;
+        SubHandler subHandler;
+
         public JThread(String name){
             super();
             this.name=name;
         }
 
+        public SubHandler getHandler(){
+            return subHandler;
+        }
         @Override
         public void run() {
             super.run();
-            Log.d("AAA", "thread: "+Thread.currentThread().getId());
-            for(int i=0;i<100;i++){
-//                Log.d("AAA", "run: "+i);
-                Message msg=Message.obtain();
-                msg.arg1=i;
-                msg.obj=name;
+            subHandler=new SubHandler();
+        }
+    }
 
-                mHandler.sendMessage(msg);
-            }
+    class SubHandler extends Handler{
+        public SubHandler(){
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+        }
+    }
+
+    class MainHandler extends Handler{
+        public MainHandler(){
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
         }
     }
 }
