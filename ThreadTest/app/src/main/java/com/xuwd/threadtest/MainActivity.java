8b2ff0ffad4b6b72c87babb47b322a164a3a7b90
goldmap <xuwd68@qq.com>
2019-08-27 19:54:53 +0800

    thread

--- ThreadTest/app/src/main/java/com/xuwd/threadtest/MainActivity.java
+++ ThreadTest/app/src/main/java/com/xuwd/threadtest/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -41,9 +43,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch(view.getId()){
             case R.id.btnMainToSub:
-
+                Message msg=jThread.getHandler().obtainMessage();
+                msg.what=1;
+                Bundle bundle=new Bundle();
+                String curThread=Thread.currentThread().getName();
+                bundle.putString("thread",curThread);
+                bundle.putString("message","Main to sub");
+                msg.obj=bundle;
+                jThread.getHandler().handleMessage(msg);
+                Toast.makeText(getBaseContext(),"Y",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btnSubToMain:
+                jThread.sendMessage("Aha");
                 break;
         }
     }
@@ -63,7 +74,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         @Override
         public void run() {
             super.run();
+            //Looper looper=Looper.myLooper();
+            Looper.prepare();
+
+            Message msg=mainHandler.obtainMessage();
+            msg.what=1;
+            Bundle bundle=new Bundle();
+            String curThread=Thread.currentThread().getName();
+            bundle.putString("thread",curThread);
+            bundle.putString("message","in run()");
+            msg.obj=bundle;
+            mainHandler.sendMessage(msg);
+
             subHandler=new SubHandler();
+
+            sendMessage("CCC");
+        }
+
+        public void sendMessage(String ms){
+            Message msg=mainHandler.obtainMessage();
+            msg.what=1;
+            Bundle bundle=new Bundle();
+            String curThread=Thread.currentThread().getName();
+            bundle.putString("thread",curThread);
+            bundle.putString("message","in sendMesdsage"+ms);
+            msg.obj=bundle;
+            mainHandler.sendMessage(msg);
         }
     }
 
@@ -73,7 +109,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         @Override
         public void handleMessage(@NonNull Message msg) {
-            super.handleMessage(msg);
+            switch(msg.what){
+                case 1:
+                    String str="SubHandler Thread ("+Thread.currentThread().getName()+") receive a message from: [";
+
+                    Bundle bundle=(Bundle) msg.obj;
+                    str+=bundle.getString("thread")+"] with information: ";
+                    str+=bundle.getString("message");
+                    Log.d("AAA", str);
+            }
+            //super.handleMessage(msg);
         }
     }
 
@@ -83,7 +128,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         @Override
         public void handleMessage(@NonNull Message msg) {
-            super.handleMessage(msg);
+            switch(msg.what){
+                case 1:
+                    String str="MainHandler Thread ("+Thread.currentThread().getName()+") receive a message from: [";
+
+                    Bundle bundle=(Bundle) msg.obj;
+                    str+=bundle.getString("thread")+"] with information: ";
+                    str+=bundle.getString("message");
+                    Log.d("AAA", str);
+            }
+            //super.handleMessage(msg);
         }
     }
 }
