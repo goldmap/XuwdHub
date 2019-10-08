db55c690a14bc3698ba899fe653569b44326b40a
goldmap <42493201+goldmap@users.noreply.github.com>
2019-10-08 15:05:23 +0800

    j

---
+++ WenWeb/WenWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WenWeb.Startup))]
+namespace WenWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
