5492125d41d7cee15e4d4344c201b90c110e6329
goldmap <xuwd68@qq.com>
2019-10-09 22:10:17 +0800

    j

---
+++ WebTest/WebTest/WebTest/Startup.cs
@@ -0,0 +1,62 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using WebTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //var connection = @"Server=(localdb)\mssqllocaldb;Database=WebTest.Blogs.Xuwd;Trusted_Connection=True;ConnectRetryCount=0";
+            // services.AddDbContext<BloggingContext>(options => options.UseSqlServer(connection));
+            services.AddDbContext<BloggingContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+        }
+    }
+}
---
+++ WebTest/WebTest1/WebTest/Migrations/20190910142401_initial.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebTest.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JBlogs",
+                columns: table => new
+                {
+                    JBlogId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Subject = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JBlogs", x => x.JBlogId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JImages",
+                columns: table => new
+                {
+                    JImageId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Subject = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JImages", x => x.JImageId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JPosts",
+                columns: table => new
+                {
+                    JPostId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true),
+                    BlogId = table.Column<int>(nullable: false),
+                    JBlogId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JPosts", x => x.JPostId);
+                    table.ForeignKey(
+                        name: "FK_JPosts_JBlogs_JBlogId",
+                        column: x => x.JBlogId,
+                        principalTable: "JBlogs",
+                        principalColumn: "JBlogId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JPosts_JBlogId",
+                table: "JPosts",
+                column: "JBlogId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JImages");
+
+            migrationBuilder.DropTable(
+                name: "JPosts");
+
+            migrationBuilder.DropTable(
+                name: "JBlogs");
+        }
+    }
+}
